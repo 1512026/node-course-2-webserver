109a8c6fbaa7d89a0e5941972e0f366603fed191
Lương Thái Bảo <thaibao1997@gmail.com>
2018-03-25 16:02:58 +0700

    Cofiguartio for heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const hbs=require('hbs');
 
+const port = process.env.PORT || 3000;
+
 let app=express();
 hbs.registerPartials(__dirname+'/views/partials');
 hbs.registerHelper('getCurrentYear',()=> new Date().getFullYear());
@@ -34,4 +36,4 @@ app.get('/about',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{console.log("server is up")});
+app.listen(port,()=>{console.log("server is up port: " + port)});
